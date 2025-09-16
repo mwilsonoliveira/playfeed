fa6d0ecf02a42c27b9138247907c92d04694adf1
Marcos Wilson <mwilson.oliveira@gmail.com>
2025-09-16 02:21:32 +0000

    chore: removing unused code

--- app/api/news/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { getNews } from "@/lib/getNews";
-
-export const revalidate = 3600; // 1h
-
-export async function GET() {
-  const news = await getNews();
-  return NextResponse.json(news);
-}
